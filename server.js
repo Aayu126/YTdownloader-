f0b8e8b10b2c2d97dc151686c438ba75114a9dcd
AYUSH CHAVAN <ayushchavan1980@gmail.com>
2025-08-14 15:33:25 +0530

    cchange

--- server.js
+++ server.js
@@ -41,10 +41,9 @@ if (proxyUrl) {
 }
 
 // Define request options to use the proxy agent and a user agent
-// FIX: Changed 'agent' to 'client' to fix the unsupported opts.agent error
 const requestOptions = {
     requestOptions: {
-        client: proxyAgent,
+        client: proxyAgent, // FIX: Changed 'agent' to 'client' to fix the unsupported opts.agent error
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
         }
